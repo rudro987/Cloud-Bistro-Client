8da84976dd51106ad39b16aea8a2ca7d7bf47d18
Tanvir Rudro <rudro987@gmail.com>
2024-04-18 21:40:34 +0600

    Carousel slider added

--- src/Pages/Shared/NavBar.jsx
+++ src/Pages/Shared/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     </>
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-2xl ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,7 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home/Home";
 
   export const router = createBrowserRouter([
     {
